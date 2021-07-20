ec6faa6b123a68b85cf19af7283de13487ae7f07
ajrominger <ajrominger@gmail.com>
2021-07-20 12:11:53 -0400

    added general package stuff

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(abbaGapR)
+
+test_check('abbaGapR')
---
+++ tests/testthat/test_getSeqs.R
@@ -0,0 +1,12 @@
+# some good IDs to use in testing
+
+cast <- list(id = '1331395866',
+             acc = 'MG220179',
+             latlon = '49.158 N 121.594 W',
+             sp = 'Castilleja rupicola')
+
+murph <- list(id = '1679378317',
+              acc = 'MK277062',
+              latlon = NA,
+              sp = 'Drosophila murphyi')
+
