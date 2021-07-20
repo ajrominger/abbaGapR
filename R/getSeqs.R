9dd1287dc2b02c64368a58f7dddf691b3e9d9590
ajrominger <ajrominger@gmail.com>
2021-07-20 11:39:06 -0400

    got a working version of funciton to get genbank sequences (using `rettype = 'gbc'`)

--- R/getSeqs.R
+++ R/getSeqs.R
@@ -1,127 +1,83 @@
-getWhatWeWant <- function(entrezOut) {
-    # linearize the XML mess
-    entrezOut <- unlist(XML::xmlToList(entrezOut))
+getGenBankSeqs <- function(id) {
+    # get the raw record
+    raw <- rentrez::entrez_fetch(db = 'nuccore', id = id,
+                                 rettype = 'gbc', retmode = 'xml')
 
-    # keywords in XML hierarchy to find wanted info
-    weWantDirect <- list(
-        'taxon_name' = 'Org-ref_taxname',
-        'date' = 'Seqdesc_create-date.Date.Date_std.Date-std.Date-std_year',
-        'seq' = 'Seq-data_ncbi2na.NCBI2na',
-        'pub' = c('ArticleId_pubmed.PubMedId', 'ArticleId_doi.DOI'),
-        'accession' = 'Textseq-id_accession', # this could have multiple matches, we want the first only
-        'genomic_region' = c(
-            'SeqFeatData_prot.Prot-ref.Prot-ref_name.Prot-ref_name_E',
-            'SeqFeatData_rna.RNA-ref.RNA-ref_type.value',
-            'Seq-feat_comment'
-        ),
-        'primer_forward' = 'PCRReaction.PCRReaction_forward.PCRPrimerSet.PCRPrimer.PCRPrimer_seq.PCRPrimerSeq',
-        'primer_reverse' = 'PCRReaction.PCRReaction_reverse.PCRPrimerSet.PCRPrimer.PCRPrimer_seq.PCRPrimerSeq')
+    # convert XML to list
+    rawList <- XML::xmlToList(raw)
 
-    # keyword `attrs.value` in XML; the wanted info will be at the index + 1
-    # (thus `PP` aka `++` aka `index + 1`)
-    weWantPP <- list(
-        'latlon' = 'lat-lon',
-        'location' = 'country',
-        'specimen_id' = 'specimen-voucher'
+    # get easy to retrieve data
+    dat <- data.frame(
+        accession = rawList$INSDSeq$`INSDSeq_accession-version`,
+        species = rawList$INSDSeq$INSDSeq_organism,
+        date = rawList$INSDSeq$`INSDSeq_create-date`,
+        pubmed = rawList$INSDSeq$INSDSeq_references$INSDReference$
+            INSDReference_pubmed
     )
 
-    # loop over keywords and extract info
-    info1 <- lapgrep(weWantDirect, crazyGREP, entrezOut)
-    info2 <- lapgrep(weWantPP, grepPP, entrezOut)
+    # get taxonomy
+    tax <- data.frame(taxonomy = rawList$INSDSeq$INSDSeq_taxonomy)
 
-    return(rbind(info1, info2))
+    # extract feature table
+    featTab <- rawList$INSDSeq$`INSDSeq_feature-table`
+
+    # add data from feature table
+    dat <- cbind(dat, parseFeatTab(featTab))
+
+    # extract sequence and format it as FASTA
+    dnaFASTA <- formatFASTA(dat$accession,
+                            rawList$INSDSeq$INSDSeq_sequence)
+
+    return(list(data = dat, taxonmy = tax, dna = dnaFASTA))
 }
 
-crazyGREP <- function(p, x) {
-    grep(paste(p, collapse = '|'), names(x))
+
+
+parseFeatTab <- function(featTab) {
+    featTab <- unlist(featTab)
+
+    # gather info into data.frame
+    info <- data.frame(
+        # info about genomic region
+        # regionType = featTab['INSDFeature.INSDFeature_key'],
+        region = extractFeatByName('gene', featTab),
+        product = extractFeatByName('product', featTab),
+        organelle = extractFeatByName('organelle', featTab),
+        region_note = extractFeatByName('note', featTab),
+
+        # info about geographic location
+        latlon = extractFeatByName('lat_lon', featTab),
+        locality = extractFeatByName('country', featTab),
+
+        # info about specimen
+        coll_date = extractFeatByName('collection_date', featTab),
+        coll_by = extractFeatByName('collected_by', featTab),
+        specimen_id = extractFeatByName('specimen_voucher', featTab)
+    )
+
+    return(info)
 }
 
-grepPP <- function(p, x) {
-    grep(paste(p,  collapse = '|'), x) + 1
-}
+extractFeatByName <- function(name, featTab) {
+    index <- which(grepl('INSDQualifier_name', names(featTab)) &
+                       featTab == name) + 1
 
-lapgrep <- function(x, gfun, dat) {
-    ii <- 1:length(x)
+    if(length(index) == 0) {
+        out <- NA
+    } else {
+        out <- as.character(featTab[index])
+        out <- unique(out)
 
-    out <- lapply(ii, function(i) {
-        p <- x[[i]]
-        jj <- gfun(p, dat)
-        val <- dat[jj]
-        if(length(val) == 0) val <- NA
-
-        o <- try(data.frame(field = names(x)[i],
-                            # match_term = paste(p, collapse = '|'),
-                            value = val))
-        if('try-error' %in% class(o)) browser()
-
-        return(o)
-    })
-
-    out <- do.call(rbind, out)
-    rownames(out) <- NULL
+        if(out == '') out <- NA
+    }
 
     return(out)
 }
 
-
-#
-# new <- rentrez::entrez_fetch(db = 'nuccore', id = '1490508',
-#                              rettype = 'native', retmode = 'xml',
-#                              parsed = TRUE)
-#
-#
-old <- rentrez::entrez_fetch(db = 'nuccore', id = '1331395866',
-                             rettype = 'acc', retmode = 'text')
-
-foo <- entrez_summary('nuccore', '1679378317')
-names(foo)
-
-for(i in names(foo)) {
-    print(extract_from_esummary(foo, i))
+formatFASTA <- function(dbID, dnaSeq) {
+    paste0('>', dbID, '\n', dnaSeq)
 }
 
-entrez_fetch
 
-x <- getWhatWeWant(boo)
-
-
-sort(unique(x$value[x$field == 'accession']))
-
-
-library(rentrez)
-library(XML)
-
-foo <- rentrez::entrez_search(db = 'nuccore', term = 'Drosophila murphyi[ORGN]',
-                              retmax = 1000)
-foo$ids
-
-
-
-raw <- rentrez::entrez_fetch(db = 'nuccore', id = '1679378317',
-                             rettype = 'gbc', retmode = 'xml')
-raw <- readLines('~/Desktop/sequence.gbc.xml')
-rawList <- unlist(XML::xmlToList(raw))
-allTax <- rawList[grep('Org-ref_taxname', names(rawList))]
-names(allTax) <- NULL
-length(unique(allTax))
-head(allTax)
-
-rawFASTA <- rentrez::entrez_fetch(db = 'nuccore', id = '403062956',
-                             rettype = 'fasta')
-cat(rawFASTA)
-
-
-boo <- cbind(names(rawList), as.character(rawList))
-x <- strsplit(boo[, 1], '\\.')
-n <- max(sapply(x, length))
-x <- lapply(x, function(y) {
-    y <- c(y, rep('', n - length(y)))
-    return(y)
-})
-
-boo <- cbind(do.call(rbind, x), boo[, 2])
-write.csv(boo, 'boo2.csv', row.names = FALSE)
-
-z <- list(a = list(a1 = 1, a2 = 2), b = 3, c = list(c1 = 4, c2 = 5))
-unlist(z)
-
+getGenBankSeqs('1331395866')
+getGenBankSeqs('1679378317')
